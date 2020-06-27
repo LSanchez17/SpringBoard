ba3f894f6ef5b7968c8bc5c50af5f36141ec9fe8
LSanchez17 <galaxyplayeremail@gmail.com>
2020-06-27 09:48:30 -0400

    Update app.py
    added session storage

--- python/flask/survey/app.py
+++ python/flask/survey/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
-# from Flask_Debugtoolbar import DebugToolbarExtension
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+from Flask_Debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 
-# app.config['SECRET_KEY'] = 'SOMETHING'
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-# debug = DebugToolbarExtension(app)
+app.config['SECRET_KEY'] = 'SOMETHING'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 
 RESPONSES = []
@@ -15,36 +15,44 @@ RESPONSES = []
 def home():
     """Home Page"""
     root = True
+    session['responses'] = []
     return render_template('base.html', show=root) 
 
 @app.route('/questions/<int:n>')
 def questions(n):
     """Question Handling and basic logic"""
-    """My Venv could not locate the debugtoolbar, even though 
-       it was installed in the libraries, strange.  So withouth it
-       I can't use the following code, as it depends on sessions
-    """
-    # if len(RESPONSES) is None:
-    #     return redirect('/')
+
+    if len(RESPONSES) is None:
+        return redirect('/')
     
-    # if len(RESPONSES) == len(satisfaction_survey.questions):
-    #     return redirect('/done')
+    if len(RESPONSES) == len(satisfaction_survey.questions):
+        return redirect('/done')
     
-    # if len(RESPONSES) != satisfaction_survey.questions[n]:
-    #     flash(f'Invalid Attempt! No cheating!')
-    #     return redirect(f'/questions/{n}')
+    if len(RESPONSES) != satisfaction_survey.questions[n]:
+        flash(f'Invalid Attempt! No cheating!')
+        return redirect(f'/questions/{n}')
     
     return render_template('questions.html', n=n, satisfaction_survey=satisfaction_survey)
 
 @app.route('/questions/<int:n>', methods=['POST'])
 def answered(n):
     """Saves user responses"""
+
     radio_val = request.form['option']
+    
+    """Adding values to the session"""
+    session_value = session['survey']
+    session_value.append(radio_val)
+    session['survey'] = session_value
+
     RESPONSES.append(radio_val)
     if len(RESPONSES) != 4:
         return redirect(url_for('questions', n=n+1))
     else:
         return render_template('results.html', responses = RESPONSES)
+
+
+
 
 @app.route('/done')
 def finished():
