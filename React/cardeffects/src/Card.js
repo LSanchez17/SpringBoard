45e585ddb77fe2d6cc6bed8e1647b9df6558d150
LSanchez17 <galaxyplayeremail@gmail.com>
2020-12-12 20:24:44 -0500

    useRef with cardAPI

--- React/cardeffects/src/Card.js
+++ React/cardeffects/src/Card.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Card = ({image, value, suit}) => {
+    console.log('image', image)
+    console.log('value', value)
+    console.log('suit', suit);
     return(
         <div>
             <h3>{value} of {suit}</h3>
--- React/cardeffects/src/Deck.js
+++ React/cardeffects/src/Deck.js
@@ -1,70 +1,88 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 import Card from './Card';
 
 const Deck = () => {
+    //set up deck
     const [deck, setDeck] = useState(null);
+    //empty array to store cards
     const [cards, setCard] = useState([]);
-    const [drew, setDraw] = useState(false);
+    //set up a way to draw cards each second
+    const [autoDraw, setAutoDraw] = useState(false);
+    const timer = useRef(null);
+
 
     useEffect(() => {
-        //assign async function to retrive a new deck
-        //Then assign the deck property 
-        async function getNewDeck() {
-            const res = await axios.get('https://deckofcardsapi.com/api/deck/new/');
-            console.log(res.data);
+        async function drawDeck() {
+            //gets new deck from axios request
+            let res = await axios.get('https://deckofcardsapi.com/api/deck/new/');
+            //set the deck state to the response
             setDeck(res.data);
         }
 
-        getNewDeck();
+        //Call the synchrounous function
+        drawDeck(); 
+        //We depend on setDeck, to redraw a new deck, we'd initialize the deck to null again
+        //which then hooks onto useEffect()
     }, [setDeck]);
 
     useEffect(() => {
-        async function drawCard() {
-            const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`);
-            
+        const newCard = async() => {
+            let {deck_id} = deck;
+
+            let res = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
+
             if(res.data.remaining === 0){
-                throw new Error('Out of Cards');
+                throw new Error('No cards left');
             }
 
-            console.log('Drawn!')
-            setCard(cards => [
-                ...cards, 
-                {
-                    image: res.data.cards[0].image,
-                    value: res.data.cards[0].value,
-                    suit: res.data.cards[0].suit
+            setCard(cards =>
+                [...cards, 
+                {image: res.data.cards[0].image,
+                 suit: res.data.cards[0].suit,
+                 value: res.data.cards[0].value
                 }
-            ]);
+            ])
         }
         
-        drawCard();
+        if(autoDraw && !timer.current){
+            timer.current = setInterval(async () => {
+                await newCard();
+            }, 1500);
+        }
 
-    }, [drew, setDraw]);
+        return () => {
+            clearInterval(timer.current);
+            timer.current = null;
+        }
+    }, [autoDraw, setAutoDraw, deck]);
 
-    const showCards = cards.map((card, idx) => {
-        //Rendering of a card from the list of drawn cards
-        return <Card key={idx} image={card.value} suit={card.suit} />
-    });
-
-    const drawNewCard = () => {
-        //invert state logic and return it as the new state
-        setDraw(drew => !drew);
+    const activateAutoDraw = () => {
+        setAutoDraw(autoDraw => !autoDraw);
     }
+
+    const displayCard = cards.map((card,idx) => {
+        return <Card image={card.image} value={card.value} suit={card.suit} key={idx}/>
+    });
 
     return(
         <div>
-            {
-            deck 
-            ? 
+            <p>Deck Drawing Game</p>
+            {deck 
+             ?
             <div>
-                <p>Your deck is {deck.deck_id}, with {deck.remaining} remaining</p> 
-                <button onClick={drawNewCard}>Draw new Card</button>
+                <p>Deck id: {deck.deck_id}, cards remaining: {deck.remaining}</p>
+                <button onClick={activateAutoDraw}>Start drawing cards randomly</button>
             </div>
-            : 
-            <h3>Loading...</h3> 
+             :
+             <h1>Loading...</h1>
             }
-            <div>{showCards}</div>
+            {cards.length !== 0
+                 ?
+                 displayCard
+                 :
+                 <h2>No cards drawn yet</h2>
+            }
         </div>
     )
 }
